3f4be2181d64deeb1d1164edf8f2cbaf182b0dfb
Thomas Lopes | Stone <thomas.lopes@stone.com.br>
2019-10-22 13:08:48 -0300

    feat: add buefy

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,21 @@
+// Vue
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+
+// Font Awesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(faUserSecret);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+// Buefy
+import Buefy from "buefy";
+Vue.use(Buefy, {
+  defaultIconComponent: "vue-fontawesome",
+  defaultIconPack: "fas"
+});
 
 Vue.config.productionTip = false;
 
