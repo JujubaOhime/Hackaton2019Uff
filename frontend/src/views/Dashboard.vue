41212f1b96f3796260db93842f705a1ee4b21be6
Thomas Lopes | Stone <thomas.lopes@stone.com.br>
2019-10-22 17:59:49 -0300

    feat: separate dash

---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <section class="section">
+      <div class="container">
+        <div class="tile is-ancestor">
+          <div class="tile is-parent is-4">
+            <article class="tile is-child box">
+              <TodoList />
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+// Components
+import TodoList from "@/components/Todo/TodoList";
+
+export default {
+  name: "Dashboard",
+  components: { TodoList }
+};
+</script>
